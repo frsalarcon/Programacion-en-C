9e626b4a871647f14cc0b2acc04e162ffb1cdfab
frsalarcon <66811000+frsalarcon@users.noreply.github.com>
2020-07-22 01:13:47 -0400

    Add files via upload

---
+++ 1.Holamundo.c
@@ -0,0 +1,12 @@
+/* Programa de "Hellow World" */
+/* Todo lo que comienza con # es procesado por el pre-procesador antes que se compile el programa  */
+/* Para añadir librerias */
+/* stdio: standar  input output*/
+#include<stdio.h>
+/* Función main inicia la ejecución del programa*/ 
+int main(){
+/* \n new line. Caracter de escape */
+    printf("Hola Mundo prueba \n");  
+/*  return El programa ha finalizado con exito*/
+    return 0;
+}
---
+++ 2.Suma.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int num1;
+    int num2;
+    int resultado;
+    printf("Ingrese el primer  valor:\n");
+    /* Para lectura de datos: scanf*/
+    /* scanf tiene dos argumentos: "tipo de archivo" {ej %d:entero decimal} */
+    /* argumento 2: "&" operador de direccion, ruta o variable donde se almacena el valor */
+    scanf("%d", &num1);   
+    printf("Ingrese el segundo valor:\n");
+    scanf("%d",&num2);
+    /*Definimos una funcion*/
+    resultado = num1 + num2;
+    printf(" El resultado de la suma es: %d\n ", resultado);
+    return 0;
+}
